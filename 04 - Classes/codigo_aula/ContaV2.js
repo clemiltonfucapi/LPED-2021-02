547992e5b8ec3e6601faec2cd8d887f8969dcdeb
clemilton <clemilton.fucapi@gmail.com>
2021-09-23 20:38:32 -0400

    exercicios

--- 04 - Classes/codigo_aula/ContaV2.js
+++ 04 - Classes/codigo_aula/ContaV2.js
@@ -1,6 +1,12 @@
 
 
 class Conta{
+    /* declaração dos atributos */
+    nome;
+    cpf;
+    agencia;
+    saldo;
+
     /* método construtor */
     constructor(nome,cpf,agencia,saldo){
         this.nome = nome;
@@ -8,50 +14,36 @@ class Conta{
         this.agencia = agencia;
         this.saldo = saldo
     }
-    /* retorna uma string, com as info. do obj*/
-    toString(){
+   
+     /* retorna uma string, com as info. do obj*/
+     toString(){
         return `Nome: ${this.nome}\n`+
                 `CPF: ${this.cpf}\n`+
                 `Agencia: ${this.agencia}\n`+
                 `Saldo: ${this.saldo}\n`
     }
-    /* metodo para fazer um saque de um valor */
-    saque(valor){
-        //caso nao tenha valor suficiente
-        if(valor > this.saldo){
-            console.log("Não tem saldo suficiente!")
-            return;
-        }
-        //caso valor invalido
-        if( valor <= 0 ){
-            console.log("Valor invalido")
-            return;
-        }
-        this.saldo -= valor;
-        console.log("Saque realizado com sucesso");
-    }
 
-    
+    /* método deposita(): aumenta o saldo */
     deposita(valor){
-        if(valor <= 0 ){
-            console.log('Valor invalido!')
-            return ;
+        //validação do valor
+        if(valor <=0) {
+            console.log('Valor invalido!');
+            return ; // quebrar o código 
         }
-        //aumentar o saldo
+        // aumentar o saldo
         this.saldo += valor;
-        console.log(`Foi depositado ${this.saldo} reais na sua conta ${this.nome}`)
+        console.log(`Foi depositado ${valor} reais na sua conta ${this.nome}`)
     }
-
 
 }
-let c1 = new Conta('Ricardo',111111,1001,0)
+let c1 = new Conta('Ricardo',111111,1001,50)
 let c2 = new Conta('Alice',2323232,1111,0)
+//Exercicio : Instancie uma nova conta
 
-c1.deposita(50)
-c2.deposita(150)
+//deposita 70 reais em c1
+c1.deposita(70);
+c2.deposita(80);
+c1.deposita(-40)
 
-c1.saque(25)
-c2.saque(50)
-
-console.log( 'Conta 1: \n'+c1.toString() )
-console.log( 'Conta 2: \n'+c2.toString() )
+console.log(c1.toString())
+console.log(c2.toString())
